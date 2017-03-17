08d60afe6c32b2f0437abc7fa53c45a08864159a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-17 12:58:00 -0700

    Finished ajax get requests

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -3,28 +3,40 @@
 
 // PART 1: SHOW A FORTUNE
 
-function showFortune(evt) {
-
+function showFortune(fortune) { // fortune is returned by '/fortune'
     // TODO: get the fortune and show it in the #fortune-text div
+    $('#fortune-text').html(fortune);
 }
 
-$('#get-fortune-button').on('click', showFortune);
+function updateFortune() {
+    $.get('/fortune', showFortune);  
+}
+
+$('#get-fortune-button').on('click', updateFortune);
 
 
 
 
 
 // PART 2: SHOW WEATHER
+function showWeather(weatherObj){
+    $('#weather-info').html("The weather will be " + weatherObj.forecast 
+                            + "\nThe current temp is " + weatherObj.temp);
+    console.log(weatherObj);
+}
 
-function showWeather(evt) {
+function updateWeather(evt) {
     evt.preventDefault();
 
     var url = "/weather.json?zipcode=" + $("#zipcode-field").val();
+    $.get(url, showWeather);
 
     // TODO: request weather with that URL and show the forecast in #weather-info
 }
 
-$("#weather-form").on('submit', showWeather);
+
+
+$("#weather-form").on('submit', updateWeather);
 
 
 
